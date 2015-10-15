aa863479cf113a250f9fcf6ef974a37991d7cc92
chenzhenwen <chenzhenwen@meiriq.com>
2015-10-15 15:11:28 +0800

    add new file

--- app/src/main/java/com/example/meiriq0717/mydemo/MainActivity.java
+++ app/src/main/java/com/example/meiriq0717/mydemo/MainActivity.java
@@ -2,15 +2,25 @@ package com.example.meiriq0717.mydemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        recyclerView = (RecyclerView) findViewById(R.id.recview);
+        LinearLayoutManager manager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(manager);
     }
 
     @Override
